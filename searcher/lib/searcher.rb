d86983e7e80bb0f733bdf50b7293ad8194536a97
Paul Grayson <paul@pololu.com>
2012-06-23 13:24:21 -0700

    minimax searcher working

--- searcher/lib/searcher.rb
+++ searcher/lib/searcher.rb
@@ -5,14 +5,27 @@ class Searcher
   end
 
   def search(state, params)
-    max = -10000
-    max_move = nil
+    search_and_eval(state,params)[0]
+  end
+
+  def search_and_eval(state, params)
+    best_eval = (params[:min] ? 10000 : -10000)
+    best_move = nil
     @move_finder.find(state).each do |move|
       result = move.result
-      if @evaluator.evaluate(result) > max
-        max_move = move
+
+      if params[:depth] == 1
+        eval = @evaluator.evaluate(result)
+      else
+        eval = search_and_eval(result, params.merge(:min => !params[:min], :depth => params[:depth]-1))[1]
+      end
+
+      if (!params[:min] && eval > best_eval) ||
+          (params[:min] && eval < best_eval)
+        best_move = move
+        best_eval = eval
       end
     end
-    return max_move
+    return [best_move, best_eval]
   end
 end
--- searcher/spec/searcher_spec.rb
+++ searcher/spec/searcher_spec.rb
@@ -5,10 +5,10 @@ shared_context "simple_tree" do
   before do
     # A GameState is a state of the game.
 
-    #        5
-    #       / \
-    #      1    6
-    #    2 3 4  78
+    #                 4(1)
+    #             /       \             \
+    #          0(1)         5(5)        8(1)
+    #    1(5) 2(0) 3(11)  6(-1) 7(5)    9(-2)
     @state = []
     20.times do |i|
       @state << double("state#{i}".to_sym)
@@ -18,9 +18,29 @@ shared_context "simple_tree" do
     @move01 = double("move01", :to_s => 'move01', :result => @state[1])
     @move02 = double("move02", :to_s => 'move02', :result => @state[2])
     @move03 = double("move03", :to_s => 'move03', :result => @state[3])
+    @move40 = double("move40", :to_s => 'move40', :result => @state[0])
+    @move45 = double("move45", :to_s => 'move45', :result => @state[5])
+    @move48 = double("move48", :to_s => 'move48', :result => @state[8])
+    @move56 = double("move56", :to_s => 'move56', :result => @state[6])
+    @move57 = double("move57", :to_s => 'move57', :result => @state[7])
+    @move89 = double("move89", :to_s => 'move89', :result => @state[9])
 
     # A MoveFinder finds a series of possible moves from a state.
-    @move_finder = double(:move_finder, :find => [@move01, @move02, @move03])
+    @move_finder = double(:move_finder)
+    @move_finder.stub(:find) do |state|
+      case state
+      when @state[0]
+        [@move01, @move02, @move03]
+      when @state[4]
+        [@move40,@move45,@move48]
+      when @state[5]
+        [@move56,@move57]
+      when @state[8]
+        [@move89]
+      else
+        []
+      end
+    end
 
     # An Evaluator evaluates GameStates
     @evaluator = double(:evaluator)
@@ -34,6 +54,18 @@ shared_context "simple_tree" do
         0
       when @state[3]
         11
+      when @state[4]
+        1
+      when @state[5]
+        5
+      when @state[6]
+        -1
+      when @state[7]
+        5
+      when @state[8]
+        1
+      when @state[9]
+        -2
       end
     end
 
@@ -49,6 +81,6 @@ describe 'Searcher', 'initialize' do
   end
 
   it "should find a good move at depth 2" do
-    @searcher.search(@state[5], :depth => 2).should == @move03
+    @searcher.search(@state[4], :depth => 2).should == @move40
   end
 end
